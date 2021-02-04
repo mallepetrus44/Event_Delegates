6586d6b05ae5959cf22e9ce563f6b7ea7e67fe61
Mathijs van de Kerkhof <mathijs.vandekerkhof@itvitaelearning.nl>
2021-02-04 09:37:19 +0100

    Add project files.

---
+++ Event_Delegates_Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+delegate void MeDelegate();
+
+namespace Event_Delegates_Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            InvokeTheDelegate(Foo);
+            InvokeTheDelegate(new MeDelegate(Goo));
+        }
+
+        static void InvokeTheDelegate(MeDelegate deler)
+        {
+            deler();
+        }
+
+        static void Foo()
+        {
+            Console.WriteLine("Foo()");
+        }
+
+        static void Goo()
+        {
+            Console.WriteLine("Goo()");
+        }
+    }
+}
